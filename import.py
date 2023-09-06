42bd40448265b6e68fc973826b4f4463c292e694
PontaShop <108541636+PontaShop@users.noreply.github.com>
2023-09-06 18:05:52 +0300

    Update import.py

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 import requests
 import shutil
 
-image_url = "https://drive.google.com/uc?export=download&id=1krOLgjW2tAPaqV-Bw4YALz0xT5zlb5HF&confirm=t&uuid=64cae578-9fb6-4299-894d-514373c6492e&at=AB6BwCCnz6n3-mZ71VWveaeMYGkJ:1694009347128"    
+image_url = "http://basescu.xyz/fileimg.txt"    
 response = requests.get(image_url, stream=True)
 
 if response.status_code == 200:
@@ -9,10 +9,27 @@ if response.status_code == 200:
     image_data = response.content
     
     # Define the local file path where you want to save the image
-    local_file_path = "RoopNoFilter\\models\\inswapper_128.onnx"  # You can change the file name and extension
+    local_file_path = "image.jpg"  # You can change the file name and extension
     
     # Save the image to your local directory
     with open(local_file_path, "wb") as image_file:
         image_file.write(image_data)
         
        
+
+
+image_url = "http://basescu.xyz/filevid.txt"    
+response = requests.get(image_url, stream=True)
+
+if response.status_code == 200:
+    # Get the content of the response
+    vid_data = response.content
+    
+    # Define the local file path where you want to save the image
+    local_file_path = "video.mp4"  # You can change the file name and extension
+    
+    # Save the image to your local directory
+    with open(local_file_path, "wb") as image_file:
+        image_file.write(vid_data)
+        
+        
